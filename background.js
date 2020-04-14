8bcc2346a8daa77b6c5c65066fbf07e613e212e9
Tom Critchlow <tjcritchlow@gmail.com>
2020-04-14 14:07:24 -0400

    Added code for welcome.html on first install

--- background.js
+++ background.js
@@ -6,7 +6,17 @@ chrome.browserAction.setPopup({popup:''});  //disable browserAction's popup
 
 chrome.browserAction.onClicked.addListener(()=>{
   chrome.tabs.create({url:'options.html'});
-}); 
+});
+
+// Check whether new version is installed
+chrome.runtime.onInstalled.addListener(function(details){
+  if(details.reason == "install"){
+    chrome.tabs.create({url:'welcome.html'});
+  }else if(details.reason == "update"){
+      var thisVersion = chrome.runtime.getManifest().version;
+      console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+  }
+});
 
 
 
