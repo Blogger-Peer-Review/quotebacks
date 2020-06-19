ba4fa8687c7408197a857e579331261756450148
Tom Critchlow <tjcritchlow@gmail.com>
2020-06-19 16:10:45 -0400

    Remove bloat from background.js

--- background.js
+++ background.js
@@ -8,28 +8,15 @@
 // });
 
 
-chrome.storage.local.get(null, function(items) {
-  allKeys = Object.keys(items);
-});
+//chrome.storage.local.get(null, function(items) {
+//  allKeys = Object.keys(items);
+//});
 
 chrome.commands.onCommand.addListener(function(command) { 
   copyquote();
 });
 
 chrome.browserAction.setPopup({popup:''});  //disable browserAction's popup
-
-
-// proof of concept that can watch for pages in DB then:
-// retrieve serializedhighlights
-// send message to page to deserialize highlights
-//requires webNavigation permission in manifest.json
-/*
-chrome.webNavigation.onCompleted.addListener(function(e) {
-  if (allKeys.includes(e.url)){
-    console.log("page in DB");
-    }
-  });
-*/
 
 chrome.browserAction.onClicked.addListener(()=>{
   chrome.tabs.create({url:'options.html'});
